856ae92b4c60460b0d6beef75a19853038945b93
isaac <isaac_castro_25@outlook.com>
2022-08-02 08:19:32 -0500

    fdffdffxxdfdfvdsf

--- routes/headphonesR.js
+++ routes/headphonesR.js
@@ -10,9 +10,11 @@ headphonesRouter.get('/', async(req, res)=>
       /* const xbox = await Product.findById('62e0912605b0616b74a76971');
       const proX = await Product.findById('62e09a4871896d371d829a75');
       const xG435 = await Product.findById('62e0a30071896d371d829a7a'); */
-      const lmao = 5;
+      const proX = await Product.find({quantity:15});
+
      
-      res.render('./headphones/indexV',{lmao} /* ,
+     
+      res.render('./headphones/indexV',{proX} /* ,
       {
          xbox,
          proX,
